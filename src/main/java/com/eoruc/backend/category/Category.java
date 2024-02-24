12d21aa3caae4c69502f0e60297382a5d7b4a37b
eoruc <70223835+eoruc@users.noreply.github.com>
2024-02-24 20:00:07 +0300

    Implemented category table

---
+++ src/main/java/com/eoruc/backend/category/Category.java
@@ -0,0 +1,33 @@
+package com.eoruc.backend.category;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "\"categories\"")
+@ToString
+public class Category implements Serializable {
+  private static final long serialVersionUID = -3521667763938519924L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotBlank(message = "categoryName is mandatory")
+  private String categoryName;
+
+  @Column(columnDefinition = "TIMESTAMP")
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private Long count;
+}
