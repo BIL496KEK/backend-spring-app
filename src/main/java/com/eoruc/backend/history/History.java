5de09b36fbd22d2a1cca3243612f087a109ff14b
eoruc <70223835+eoruc@users.noreply.github.com>
2024-02-24 18:25:10 +0300

    Implemented history table

---
+++ src/main/java/com/eoruc/backend/history/History.java
@@ -0,0 +1,36 @@
+package com.eoruc.backend.history;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "\"history\"")
+@ToString
+public class History implements Serializable {
+  private static final long serialVersionUID = 7801490296921909606L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotBlank(message = "link is mandatory")
+  private String link;
+
+  @NotBlank(message = "userId is mandatory")
+  private long userId;
+
+  @Column(columnDefinition = "TIMESTAMP")
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private String categoryId;
+}
