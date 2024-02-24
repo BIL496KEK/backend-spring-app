da399092d5804d0f016ada8778cfb6e9053a193d
eoruc <70223835+eoruc@users.noreply.github.com>
2024-02-24 19:42:23 +0300

    Implemented top products table

---
+++ src/main/java/com/eoruc/backend/top/TopProduct.java
@@ -0,0 +1,35 @@
+package com.eoruc.backend.top;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "\"topProducts\"")
+@ToString
+public class TopProduct implements Serializable {
+  private static final long serialVersionUID = 2620980359035329910L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotBlank(message = "link is mandatory")
+  private String link;
+
+  @Column(columnDefinition = "TIMESTAMP")
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private Long count;
+
+  private String categoryId;
+}
