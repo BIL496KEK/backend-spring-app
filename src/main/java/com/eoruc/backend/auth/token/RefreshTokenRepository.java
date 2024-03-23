5a7ce5ecd71d97bfe7eb0d1d13e3159f8fcba83a
eoruc <70223835+eoruc@users.noreply.github.com>
2024-03-23 11:12:08 +0300

    Implemented refresh token service

---
+++ src/main/java/com/eoruc/backend/auth/token/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.eoruc.backend.auth.token;
+
+import com.eoruc.backend.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByToken(String token);
+
+  @Modifying
+  int deleteByUser(User user);
+}
--- src/main/java/com/eoruc/backend/config/security/jwt/JwtUtils.java
+++ src/main/java/com/eoruc/backend/config/security/jwt/JwtUtils.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,15 +21,16 @@ public class JwtUtils {
   @Value("${app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
-  public String generateJwtToken(Authentication authentication) {
+  public String generateJwtToken(UserDetailsImpl userPrincipal) {
+    return generateTokenFromUsername(userPrincipal.getUsername());
+  }
 
-    UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
-
+  public String generateTokenFromUsername(String username) {
     return Jwts.builder()
-        .setSubject((userPrincipal.getUsername()))
+        .setSubject(username)
         .setIssuedAt(new Date())
         .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-        .signWith(key(), SignatureAlgorithm.HS256)
+        .signWith(SignatureAlgorithm.HS512, jwtSecret)
         .compact();
   }
 
